947c1726a1123699de1a33d4ed639697c4416bfe
ZiyadBatikh <z.batikh111@gmail.com>
2024-05-08 16:14:10 +0200

    Exercise building a button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello world <span>I'm Ziyad</span>
-      </Alert>
+      <Button color="warning" onClick={() => console.log("Clicked")}>
+        Mona Elmoghazi
+      </Button>
     </div>
   );
 }
