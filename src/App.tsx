a70c9ebf7b79b816028d8bccd106cf860f3969d8
ZiyadBatikh <z.batikh111@gmail.com>
2024-05-12 18:01:12 +0200

    Adding React Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ReactIcons from "./components/React-icons";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
   return (
-    <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
-    </div>
+    <ReactIcons/>
   );
 }
 
---
+++ src/components/React-icons.tsx
@@ -0,0 +1,9 @@
+import { AiFillLike } from "react-icons/ai";
+
+const ReactIcons = () => {
+  return (
+    <AiFillLike color="red" size={50} />
+  )
+}
+
+export default ReactIcons
