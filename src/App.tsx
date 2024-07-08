e21d86999e2fb8508472ca3640a935f1b85e47a5
ZiyadBatikh <z.batikh111@gmail.com>
2024-07-08 14:06:54 +0300

    Simplyfing update logic with immer (means without mutation)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import produce from 'immer';
 
 function App() {
  const [bugs, setBugs] = useState([
@@ -7,13 +8,24 @@ function App() {
  ])
 
   const handleClick = () => {
-  setBugs(bugs.map(bug => bug.id === 1 ? {...bug , fixed:true}: bug))
+  // setBugs(bugs.map(bug => bug.id === 1 ? {...bug , fixed:true}: bug))
+  
   // So to update array of objects we use the map operator.
+
+  // Updating a property in the array of object using immer
+
+  setBugs(produce(draft =>{
+    // What is "draft" Parameter ?
+    //Ans : draft is a proxy object that records the changes we will apply to bugs array of objects, imagine it is a copy of the bugs array of objects.
+   const bug=  draft.find(bug => bug.id === 1)
+   if(bug)  bug.fixed =true
+  }))
+  
      
   };
   return (
     <div>
-     
+     {bugs.map(bug => <p key={bug.id} >{bug.title} {bug.fixed ? "Fixed" : "New"}</p>)}
       <button className="btn btn-primary" onClick={handleClick}>
         Click Me
       </button>
