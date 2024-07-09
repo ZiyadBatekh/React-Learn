4e3ee3ca66000405b3e0f4079f5f81d970d1d4c3
ZiyadBatikh <z.batikh111@gmail.com>
2024-07-09 16:31:22 +0300

    Build ExpandableText Component (Summary Component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
-
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-
-const [pizza, setPizza]= useState({
-  name: "Spicy pepperoni",
-  toppings: ["Mushrom, "]
-})
-
-const handleClick = () => {
-  setPizza({
-   ...pizza , toppings:[...pizza.toppings , "Cheese"]
-  })
-}
-
   return (
     <div>
-     <p> {pizza.toppings}</p>
-     <button onClick={handleClick}>click me</button>
+      <ExpandableText maxChars={10}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate a
+        architecto ut ipsa totam, laborum provident laboriosam illo dolore saepe
+        accusamus quod possimus, laudantium temporibus cumque excepturi nam
+        voluptas facere reprehenderit! Nihil praesentium fuga voluptatum quod
+        excepturi labore expedita inventore possimus, a in laudantium ut
+        voluptate voluptatem quidem deserunt perferendis blanditiis? Ex
+        temporibus, impedit suscipit ducimus delectus possimus rerum in tenetur
+        porro ipsam nesciunt labore, eligendi sint at explicabo sapiente
+        quisquam animi harum. Sit quos eaque quidem corrupti! Aliquid,
+        distinctio optio mollitia provident fugit illo impedit laboriosam ullam
+        commodi ea adipisci ab vitae corrupti quas quaerat autem nemo
+        accusantium iusto.
+      </ExpandableText>
     </div>
   );
 }
--- src/components/React-icons.tsx
+++
@@ -1,9 +0,0 @@
-import { AiFillLike } from "react-icons/ai";
-
-const ReactIcons = () => {
-  return (
-    <AiFillLike color="red" size={50} />
-  )
-}
-
-export default ReactIcons
