b996e15e28e5d50b1ceec7b57fc5f168b382d5dc
ZiyadBatikh <z.batikh111@gmail.com>
2024-07-15 22:53:05 +0300

    4- Accessing Input Fields using the hook useRef

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,32 @@
-import React, { FormEvent } from 'react'
+import React, { FormEvent, useRef } from 'react'
 
 const Form = () => {
 
+  const nameRef = useRef<HTMLInputElement>(null)
+  const ageRef = useRef<HTMLInputElement>(null)
+
+  const person = {name:'' , age: 0};
+
   const handleSubmit = (event:FormEvent) =>{
     event.preventDefault();
-    console.log("Submitted");
+    if(nameRef.current !== null)
+      person.name = nameRef.current.value;
+    if(ageRef.current !== null)
+      person.age = parseInt(ageRef.current.value)
+
+    console.log(person);
+    
     
   }
   return (
     <form onSubmit={handleSubmit}>
     <div className="form-group mb-3">
       <label htmlFor="name">Name</label>
-      <input type="email" className="form-control" id="name" placeholder="Enter your name" />
+      <input ref={nameRef} type="text" className="form-control" id="name" placeholder="Enter your name" />
     </div>
     <div className="form-group mb-3">
       <label htmlFor="age">age</label>
-      <input type="number" className="form-control" id="age" placeholder="Password" />
+      <input ref={ageRef} type="number" className="form-control" id="age" placeholder="Password" />
     </div>
     <button type="submit" className="btn btn-primary">Submit</button>
   </form>
