f03651b67c6028692943a670a0a1089eee7a8c65
ZiyadBatikh <z.batikh111@gmail.com>
2024-07-16 15:48:57 +0300

    use the state hook to get the value of the input field (Controlled Component)

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,36 +1,49 @@
-import React, { FormEvent, useRef } from 'react'
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: 0,
+  });
 
-  const nameRef = useRef<HTMLInputElement>(null)
-  const ageRef = useRef<HTMLInputElement>(null)
-
-  const person = {name:'' , age: 0};
-
-  const handleSubmit = (event:FormEvent) =>{
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if(nameRef.current !== null)
-      person.name = nameRef.current.value;
-    if(ageRef.current !== null)
-      person.age = parseInt(ageRef.current.value)
-
     console.log(person);
     
-    
-  }
+  };
   return (
     <form onSubmit={handleSubmit}>
-    <div className="form-group mb-3">
-      <label htmlFor="name">Name</label>
-      <input ref={nameRef} type="text" className="form-control" id="name" placeholder="Enter your name" />
-    </div>
-    <div className="form-group mb-3">
-      <label htmlFor="age">age</label>
-      <input ref={ageRef} type="number" className="form-control" id="age" placeholder="Password" />
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
-  </form>
-  )
-}
+      <div className="form-group mb-3">
+        <label htmlFor="name">Name</label>
+        <input
+        value={person.name}
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          type="text"
+          className="form-control"
+          id="name"
+          placeholder="Enter your name"
+        />
+      </div>
+      <div className="form-group mb-3">
+        <label htmlFor="age">age</label>
+        <input
+        value={person.age}
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          type="number"
+          className="form-control"
+          id="age"
+          placeholder="Password"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
